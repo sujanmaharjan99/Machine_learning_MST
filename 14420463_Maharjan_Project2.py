384966e47d07fd507435a771fd55d7f0f3959d94
Sujan Maharjan <sujan@lalitmetal.com.np>
2025-11-07 12:37:38 -0600

    files for classificaiton

---
+++ 14420463_Maharjan_Project2.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+import numpy as np
+import math
+from collections import defaultdict
+import pandas as pd
+from sklearn.metrics import classification_report, accuracy_score
